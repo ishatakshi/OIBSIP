509e9698221b471a68469d44b53129ac156f179a
ishatakshi <112650616+ishatakshi@users.noreply.github.com>
2023-11-06 18:16:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed = new  Typed(".typing", {
+    strings:["Web Designer", "Web Developer", "Graphic Designer", "ML Enthusiast"],
+    typeSpeed:20,
+    BackSpeed:10,
+    // showcurser: false
+    loop: true
+
+})
+var typed = new  Typed(".new-typing", {
+    strings:["Web Designer", "Web Developer", "Graphic Designer", "ML Enthusiast"],
+    typeSpeed:20,
+    BackSpeed:10,
+    // showcurser: false
+    loop: true
+
+})
